9cfab8a692306132fe1fe63fa86fb0e340739a21
Maximo-work <maximocruztrabajo@gmail.com>
2025-11-17 15:45:04 -0300

    vuelos ultima modificacion

--- vuelo.py
+++ vuelo.py
@@ -4,20 +4,24 @@ class Vuelo:
     self.origen = origen
     self.destino = destino
     self.lista_de_pasajeros = []
+    
 
-    def agregar_pasajero(self, pasajero,reserva):
-     if pasajero in self.lista_de_pasajeros:
+
+  def agregar_pasajero(self, pasajero,reserva):
+     if self.pasajero in self.lista_de_pasajeros:
         return f"el pasajero ya se encuetra"
      elif self.codigo != reserva.vuelo_asociado:
        return f"el vuelo no coincide"
      else:
         self.lista_de_pasajeros.append(pasajero)
-        return self.lista_de_pasajeros
+        
 
-    def quitar_pasajero(self):
+  def quitar_pasajero(self, pasajero):
      if self.pasajero in self.lista_de_pasajeros:
-      self.lista_de_pasajeros.pop()
-      return self.lista_de_pasajeros
+      self.lista_de_pasajeros.remove(pasajero)
 
-    def mostrar_vuelo(self):
+
+  def mostrar_vuelo(self):
       return f"Codigo: {self.codigo}, Origen: {self.origen}, Destino: {self.destino}"
+
+
