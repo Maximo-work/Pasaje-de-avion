babfe93861875bc0bf416b1738c4ebfce8ee6b87
Maximo-work <maximocruztrabajo@gmail.com>
2025-12-01 07:13:31 -0300

    ultima entrega

--- grafo_vuelos.py
+++ grafo_vuelos.py
@@ -39,3 +39,20 @@ mi_grafo.agregar_arista('Estación 3', 'Estación 4')
 # 3. Mostrar el grafo
 print("\nRepresentación del Grafo Dirigido:")
 mi_grafo.imprimir_grafo()
+
+# 1. Agregar nodos
+mi_grafo.agregar_nodo('Estación 1')
+mi_grafo.agregar_nodo('Estación 2')
+mi_grafo.agregar_nodo('Estación 3')
+mi_grafo.agregar_nodo('Estación 4')
+
+# 2. Agregar aristas (conexiones)
+mi_grafo.agregar_arista('Estación 1', 'Estación 2')
+mi_grafo.agregar_arista('Estación 2', 'Estación 3')
+mi_grafo.agregar_arista('Estación 4', 'Estación 2')
+mi_grafo.agregar_arista('Estación 3', 'Estación 4')
+
+# 3. Mostrar el grafo
+print("\nRepresentación del Grafo Dirigido:")
+mi_grafo.imprimir_grafo()
+
