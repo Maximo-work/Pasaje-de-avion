46572ef386817a47914d6a1ba10cc3bc5dec1130
Maximo-work <maximocruztrabajo@gmail.com>
2025-12-01 07:10:50 -0300

    ultima entrega

--- reserva_pasajes.py
+++ reserva_pasajes.py
@@ -1,17 +1,25 @@
 class Reserva_Pasajes:
-    def __init__(self, numero_reserva, pasajero_asociado, asiento_asignado, vuelo_asociado, precio_pasaje):
+    def __init__(self, numero_reserva, pasajero, asiento, vuelo, precio_pasaje):
       self.numero_reserva = numero_reserva
-      self.pasajero_asociado = pasajero_asociado
-      self.asiento_asignado = asiento_asignado
-      self.vuelo_asociado = vuelo_asociado
+      self.pasajero = pasajero
+      self.asiento = asiento
+      self.vuelo = vuelo
       self.precio_pasaje = precio_pasaje
+      self.activa = True
+
+    def agregar_reserva(self):
+        if self.numero_reserva in self.numero_reserva:
+          return "El numero de reserva ya existe"
+        else:
+          self.numero_reserva.append(self.numero_reserva)
+          return self.numero_reserva
 
     def cancelar_reserva(self):
-        if self.numero_reserva is not self.numero_reserva:
-          return "El numero de reserva no coincide"
+        if self.activa:
+          self.activa = False
+          return "La reserva ha sido cancelada"
         else:
-          self.numero_reserva.remove(self.numero_reserva)
-          return self.numero_reserva
+          return "La reserva ya está cancelada"
 
 
     def actualizar_precio_pasaje(self, nuevo_precio):
@@ -20,3 +28,12 @@ class Reserva_Pasajes:
         else:
           self.precio_pasaje = nuevo_precio
           return nuevo_precio
+
+    def __str__(self):
+        """Representación legible del objeto."""
+        estado = "Activa" if self.activa else "Cancelada"
+        return f"Reserva {self.numero_reserva} | Pasajero: {self.pasajero} | Vuelo: {self.vuelo} | Estado: {estado}"
+
+
+
+   
