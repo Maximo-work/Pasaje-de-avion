a976704b2c3722494756737e086e23dcf8b3e510
Maximo-work <maximocruztrabajo@gmail.com>
2025-12-01 07:03:42 -0300

    ultima entrega

--- pasajero.py
+++ pasajero.py
@@ -1,39 +1,39 @@
-class Reserva_Pasajes:
-    def __init__(self, numero_reserva, pasajero, asiento, vuelo, precio_pasaje):
-      self.numero_reserva = numero_reserva
-      self.pasajero = pasajero
-      self.asiento = asiento
-      self.vuelo = vuelo
-      self.precio_pasaje = precio_pasaje
-      self.activa = True
+class Pasajero:
+  def __init__(self, nombre, documento, nacionalidad):
+    self.nombre = nombre
+    self.documento = documento
+    self.nacionalidad = nacionalidad
+    self.historial_de_vuelo = []
+    self.equipajes = []
+    self.equipajes_peso = 0
 
-    def agregar_reserva(self):
-        if self.numero_reserva in self.numero_reserva:
-          return "El numero de reserva ya existe"
-        else:
-          self.numero_reserva.append(self.numero_reserva)
-          return self.numero_reserva
+  def mostrar_pasajero(self):
+    print(f"Nombre: {self.nombre}")
+    print(f"Documento: {self.documento}")
+    print(f"Nacionalidad: {self.nacionalidad}")
+    print(f"Equipajes: Peso Total: {self.equipajes_peso} Kg")
+    for e in self.equipajes:
+      print(e)
 
-    def cancelar_reserva(self):
-        if self.activa:
-          self.activa = False
-          return "La reserva ha sido cancelada"
-        else:
-          return "La reserva ya está cancelada"
+  def agregar_vuelo(self, vuelo):
+    """
+     Permite agregar un vuelo al pasajero.
+    """
 
+    self.historial_de_vuelo.append(vuelo)
+    
 
-    def actualizar_precio_pasaje(self, nuevo_precio):
-        if self.precio_pasaje < 0:
-          return "El precio del pasaje no puede ser negativo"
-        else:
-          self.precio_pasaje = nuevo_precio
-          return nuevo_precio
+  def agregar_equipaje(self, equipaje):
+      """
+       Permite agregar un equipaje al pasajero.
+      """
+      
+      self.equipajes.append(equipaje)
+      self.equipajes_peso += equipaje.peso
+      
 
-    def __str__(self):
-        """Representación legible del objeto."""
-        estado = "Activa" if self.activa else "Cancelada"
-        return f"Reserva {self.numero_reserva} | Pasajero: {self.pasajero} | Vuelo: {self.vuelo} | Estado: {estado}"
+  def quitar_equipaje(self, equipaje):
+     self.equipajes.remove(equipaje)
+     self.equipajes_peso -= equipaje.peso
+     
 
-
-
-   
